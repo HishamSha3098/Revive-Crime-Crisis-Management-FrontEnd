b9b89808d2cf6be2e9d93176948edb59bbc45167
HishamSha3098 <ihthishamanu@gmail.com>
2023-08-15 16:20:04 +0530

    frontend Updations2

--- src/pages/dashboard/Staff-application.jsx
+++ src/pages/dashboard/Staff-application.jsx
@@ -121,7 +121,7 @@ export function StaffApplication() {
                     <tr key={name}>
                       <td className={className}>
                         <div className="flex items-center gap-4">
-                          <Avatar src={`${API_URL}/${table.image}`} alt={name} size="sm" />
+                          <Avatar src={`${API_URL}${table.image}`} alt={name} size="sm" />
                           <div>
                             <Typography
                               variant="small"
